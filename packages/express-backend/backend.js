d1a289f3925fd3e2c08069d88b13ee48b58e9670
hudsonschmidt <hudsonschmidt08@gmail.com>
2024-10-08 14:42:53 -0700

    Added findById

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,60 +6,72 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 const findUserByName = (name) => {
     return users["users_list"].filter(
-      (user) => user["name"] === name
+        (user) => user["name"] === name
     );
-  };
-  
-  app.get("/users", (req, res) => {
+};
+
+app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
     } else {
-      res.send(users);
+        res.send(users);
     }
-  });
+});
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+    console.log(
+        `Example app listening at http://localhost:${port}`
+    );
 });
 
 const users = {
     users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        }
     ]
-  };
+};
 
-  
