f3208ddc7a087c5e41a0fd9a3d5d3360a89fe090
hudsonschmidt <hudsonschmidt08@gmail.com>
2024-10-16 20:24:03 -0700

    Added post calls from from frontend to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -10,9 +10,39 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
   }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="container">
@@ -24,19 +54,5 @@ function MyApp() {
     </div>
   );
 }
-
-function fetchUsers() {
-  const promise = fetch("http://localhost:8000/users");
-  return promise;
-}
-
-useEffect(() => {
-  fetchUsers()
-    .then((res) => res.json())
-    .then((json) => setCharacters(json["users_list"]))
-    .catch((error) => {
-      console.log(error);
-    });
-}, []);
 
 export default MyApp;
